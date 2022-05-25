b0a060c2ab1effee59de46369cef3f96db6cab60
Violeta Pinto Leite <violetalopesalves@gmail.com>
2022-05-25 11:16:20 +0000

    Added card grid

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -3,39 +3,47 @@ import Navbar from "./navbar.jsx";
 import Jumbotron from "./jumbotron.jsx";
 import Card from "./card.jsx";
 //include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
 	return (
 		<div>
 			<Navbar />
-			<Jumbotron/>
-
-			<Card
-				img="https://picsum.photos/200"
-				title="A title"
-				text="Some text"
-				button="Click me"
-			/>
-			<Card
-				img="https://picsum.photos/200"
-				title="Another title"
-				text="Some more text"
-				button="Click me 2"
-			/>
-			<Card
-				img="https://picsum.photos/200"
-				title="Lalalal"
-				text="Lorem ipsum"
-				button="Im a button"
-			/>
-			<Card
-				img="https://picsum.photos/200"
-				title="Card title"
-				text="Some textttttttttt"
-				button="Now click"
-			/>
+			<Jumbotron />
+			<div class="row">
+				<div class="col-sm-3">
+					<Card
+						img="https://picsum.photos/200"
+						title="A title"
+						text="Some text"
+						button="Click me"
+					/>
+				</div>
+				<div class="col-sm-3">
+					<Card
+						img="https://picsum.photos/200"
+						title="Another title"
+						text="Some more text"
+						button="Click me 2"
+					/>
+				</div>
+				<div class="col-sm-3">
+					<Card
+						img="https://picsum.photos/200"
+						title="Lalalal"
+						text="Lorem ipsum"
+						button="Im a button"
+					/>
+				</div>
+				<div class="col-sm-3">
+					<Card
+						img="https://picsum.photos/200"
+						title="Card title"
+						text="Some textttttttttt"
+						button="Now click"
+					/>
+				</div>
+			</div>
 		</div>
 	);
 };
